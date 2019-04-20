a2776a8200c30643841ce94933e35a9031856616
Fulva1230 <40516157+Fulva1230@users.noreply.github.com>
2019-04-20 22:56:10 +0800

    give some commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 #include <SoftwareSerial.h>
 #include <vehicle/ConcreteVehicle.h>
 
-#define BLE_RX 4
-#define BLE_TX 2
-#define ENA 3
-#define ENB 5
-#define APIN1 7
-#define APIN2 8
-#define BPIN1 12
-#define BPIN2 13
+#define BLE_RX 4 //connect the pin with bluetooth rx pin
+#define BLE_TX 2 //connect the pin with bluetooth tx pin
+#define ENA 3 //connect to l298n ENA
+#define ENB 5 //connect to l298n ENB
+#define APIN1 7 //connect to l298n pin1
+#define APIN2 8 //connect to l298n pin2
+#define BPIN1 12 //connect to l298n pin3
+#define BPIN2 13 //connect to l298n pin4
 SoftwareSerial ble(BLE_TX, BLE_RX);
 
 ConcreteVehicle vehicle(APIN1, APIN2, ENA, BPIN1, BPIN2, ENB);
