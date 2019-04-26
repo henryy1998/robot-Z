203efb22fd42360db45200f66809b8e12ced3a07
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-04-26 20:01:41 +0800

    some change

--- src/vehicle/vehiclecommand/ServoControllCom.cpp
+++ src/vehicle/vehiclecommand/ServoControllCom.cpp
@@ -6,6 +6,8 @@
 
 void ServoControllCom::execute(AbstractVehicle &vehicle, const int16_t *params) {
     servo.write(params[0]);
+    Serial.print("servo to:");
+    Serial.println(params[0]);
 }
 
 ServoControllCom::ServoControllCom(AbstractVehicle &vehicle, const String &identifier, Servo &servo)
