5f64192394b7ef49a83495df2577e0288d93a2ac
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-05-06 15:26:35 +0800

    add the min speed

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,21 @@
 
 #define BLE_RX 4 //connect the pin with bluetooth rx pin
 #define BLE_TX 2 //connect the pin with bluetooth tx pin
-#define ENA 3 //connect to l298n ENA, connect to left motor
-#define ENB 5 //connect to l298n ENB, connect to right motor
-#define APIN1 7 //connect to l298n pin1, connect to left motor
-#define APIN2 8 //connect to l298n pin2, connect to left motor
-#define BPIN1 12 //connect to l298n pin3, connect to right motor
-#define BPIN2 13 //connect to l298n pin4, connect to right motor
+#define ENA 10 //connect to l298n ENA, connect to left motor
+#define ENB 11 //connect to l298n ENB, connect to right motor
+#define APIN1 A0 //connect to l298n pin1, connect to left motor
+#define APIN2 A1 //connect to l298n pin2, connect to left motor
+#define BPIN1 A2 //connect to l298n pin3, connect to right motor
+#define BPIN2 A3 //connect to l298n pin4, connect to right motor
 #define SERVO_PIN 9
-#define STEPPER1_PIN1 A0
-#define STEPPER1_PIN2 A1
-#define STEPPER1_PIN3 A2
-#define STEPPER1_PIN4 A3
+#define STEPPER1_PIN1 7
+#define STEPPER1_PIN2 8
+#define STEPPER1_PIN3 12
+#define STEPPER1_PIN4 13
 #define STEPPER2_PIN1 A4
 #define STEPPER2_PIN2 A5
-#define STEPPER2_PIN3 10
-#define STEPPER2_PIN4 11
+#define STEPPER2_PIN3 3
+#define STEPPER2_PIN4 5
 #define DEBUG
 SoftwareSerial ble(BLE_TX, BLE_RX);
 int16_t params[5];
