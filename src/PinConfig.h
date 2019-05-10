ea40407124e6b4620bbfd20a0c3dd3078fb38886
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-05-10 22:35:05 +0800

    change pin config to other file

---
+++ src/PinConfig.h
@@ -0,0 +1,26 @@
+//
+// Created by Fulva on 5/10/2019.
+//
+
+#ifndef ROBOT_Z_PINCONFIG_H
+#define ROBOT_Z_PINCONFIG_H
+
+#define BLE_RX 4 //connect the pin with bluetooth rx pin
+#define BLE_TX 2 //connect the pin with bluetooth tx pin
+#define ENA 3 //connect to l298n ENA, connect to left motor
+#define ENB 5 //connect to l298n ENB, connect to right motor
+#define APIN1 7 //connect to l298n pin1, connect to left motor
+#define APIN2 8 //connect to l298n pin2, connect to left motor
+#define BPIN1 12 //connect to l298n pin3, connect to right motor
+#define BPIN2 13 //connect to l298n pin4, connect to right motor
+#define SERVO_PIN 11
+#define STEPPER1_PIN1 A0
+#define STEPPER1_PIN2 A1
+#define STEPPER1_PIN3 A2
+#define STEPPER1_PIN4 A3
+#define STEPPER2_PIN1 A4
+#define STEPPER2_PIN2 A5
+#define STEPPER2_PIN3 10
+#define STEPPER2_PIN4 9
+
+#endif //ROBOT_Z_PINCONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,9 @@
 #include <vehicle/vehiclecomponent/ServoComt.h>
 #include <vehicle/vehiclecomponent/StepperComt.h>
 #include <vehicle/Vehicle.h>
+#include "PinConfig.h"
 
-#define BLE_RX 4 //connect the pin with bluetooth rx pin
-#define BLE_TX 2 //connect the pin with bluetooth tx pin
-#define ENA 3 //connect to l298n ENA, connect to left motor
-#define ENB 5 //connect to l298n ENB, connect to right motor
-#define APIN1 7 //connect to l298n pin1, connect to left motor
-#define APIN2 8 //connect to l298n pin2, connect to left motor
-#define BPIN1 12 //connect to l298n pin3, connect to right motor
-#define BPIN2 13 //connect to l298n pin4, connect to right motor
-#define SERVO_PIN 11
-#define STEPPER1_PIN1 A0
-#define STEPPER1_PIN2 A1
-#define STEPPER1_PIN3 A2
-#define STEPPER1_PIN4 A3
-#define STEPPER2_PIN1 A4
-#define STEPPER2_PIN2 A5
-#define STEPPER2_PIN3 10
-#define STEPPER2_PIN4 9
+
 //#define DEBUG
 SoftwareSerial ble(BLE_TX, BLE_RX);
 int16_t params[5];
