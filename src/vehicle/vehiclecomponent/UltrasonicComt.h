a71e2064b89a81586c7345417eebb6e4a4c570f1
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-05-30 19:09:17 +0800

    add lib

---
+++ src/vehicle/vehiclecomponent/UltrasonicComt.h
@@ -0,0 +1,18 @@
+//
+// Created by fulva on 5/30/19.
+//
+
+#ifndef ROBOT_Z_ULTRASONICCOMT_H
+#define ROBOT_Z_ULTRASONICCOMT_H
+
+#include "VehicleComt.h"
+#include <NewPing.h>
+
+class UltrasonicComt : public VehicleComt {
+public:
+    UltrasonicComt() {}
+
+
+};
+
+#endif //ROBOT_Z_ULTRASONICCOMT_H
