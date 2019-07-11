88c8635bf17f49577ee1ef0df6872deab6d94ef7
henryy1998 <henry031875@gmail.com>
2019-07-11 19:28:14 +0800

    encoder added

--- lib/PS2X_lib/examples/PS2X_Example/main.cpp
+++ lib/PS2X_lib/examples/PS2X_Example/main.cpp
@@ -10,12 +10,17 @@
 #include <vehicle/Vehicle.h>
 #include <vehicle/vehiclecomponent/ServoComt.h>
 #include "PinConfig.h"
+#include <Encoder.h>
+
+#define SENSOR_1_1 3
+#define SENSOR_1_2 4
+
+Encoder test(SENSOR_1_1,SENSOR_1_2);
 
 void setup() {
-
-
+Serial.begin(9600);
 }
 
 void loop() {
-
+Serial.println(test.read());
 }
